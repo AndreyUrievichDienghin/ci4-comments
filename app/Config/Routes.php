33881ab7288f73965dbbc35ffc47ac44919065cd
Andrey Dengin <andrey.dengin@clickon.pro>
2025-01-30 09:56:29 +0300

    fix(routes) Сделал вывод на главную страницу

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Comments::index');
 $routes->get('/comments', 'Comments::index');
 $routes->post('/comments/create', 'Comments::create');
 $routes->post('/comments/create-random', 'Comments::createBatch');
